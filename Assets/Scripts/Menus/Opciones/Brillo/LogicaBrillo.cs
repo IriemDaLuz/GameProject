d85e3c6b9a3dfcc0cc9ed1c7f1269ab6c07ca608
IriemDlzGlnd <yenduuca2005@gmail.com>
2025-04-09 03:25:28 +0100

    Hecha lógica brillo - OpcionesMenu

---
+++ Assets/Scripts/Menus/Opciones/Brillo/LogicaBrillo.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicaBrillo : MonoBehaviour
+{
+    public Slider slider;
+    public float sliderValue;
+    public Image panelBrillo;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        panelBrillo.color= new Color(panelBrillo.color.r,panelBrillo.color.g,panelBrillo.color.b, slider.value);
+    }
+
+    public void ChangeSlider(float value){
+        sliderValue=value;
+        PlayerPrefs.SetFloat("brillo", sliderValue);
+        panelBrillo.color= new Color(panelBrillo.color.r,panelBrillo.color.g,panelBrillo.color.b, slider.value);
+    }
+}
--- Assets/Scripts/Menus/Opciones/Volumen/LogicalVolumen.cs
+++ Assets/Scripts/Menus/Opciones/Volumen/LogicalVolumen.cs
@@ -31,8 +31,4 @@ public class LogicalVolumen : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        
-    }
 }
