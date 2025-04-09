290b81f19c86e76312716b6be628d4ca81e37ad4
IriemDlzGlnd <yenduuca2005@gmail.com>
2025-04-09 03:36:23 +0100

    Commit fin del dia 08/04/2025

---
+++ Assets/Scripts/Menus/Opciones/Resolucion/LogicalResolution.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+
+public class LogicalResolution : MonoBehaviour
+{
+    public TMP_Dropdown resolucionesDropdpwn;
+    Resolution[] resoluciones;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
