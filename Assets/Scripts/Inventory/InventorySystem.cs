648bbf6548523b55773d360bb8f0c1dcb440ad04
IriemDlzGlnd <yenduuca2005@gmail.com>
2025-04-28 15:38:37 +0100

    Creando script InventorySystem

---
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class InventorySystem : MonoBehaviour
+{
+    private Dictionary<InventoryItemDAta, InventoryItem> _itemDictionary;
+    public List<InventoryItem> inventory;
+
+    private void Awake()
+    {
+        inventory = new List<InventoryItem>();
+        _itemDictionary = new Dictionary<InventoryItemDAta, InventoryItem>();   
+    }
+    
+    public void Add(InventoryItemDAta itemDAta){
+        if (_itemDictionary.TryGetValue(itemDAta , out InventoryItem value)){
+            Debug.Log("SUMAR STACK EN ITEM");
+            value.AddStack();
+        } else {
+            Debug.Log("AGREGAR UN NUEVO ITEM");
+            InventoryItem newItem = new InventoryItem(itemDAta);
+            inventory.Add(newItem);
+            _itemDictionary.Add(itemDAta, newItem);
+        }
+    }
+
+    public void Remove(InventoryItemDAta itemDAta){
+        if (_itemDictionary.TryGetValue(itemDAta, out InventoryItem value)){
+            value.RemoveFromStack();
+
+            if (value.stackSize == 0){
+                inventory.Remove(value);
+                _itemDictionary.Remove(itemDAta);
+            }
+        }
+    }
+}
